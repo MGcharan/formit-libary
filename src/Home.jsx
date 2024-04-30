5691ff65802b02249d9bb7b14116dc55862c4366
giri <mgiricharan@email.com>
2024-04-30 11:43:44 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+        <div className="container" >
+      <div className="row ">
+   
+        <div className="heading text-white p-5 mt-3 mb-3 rounded-4 ">
+          <h1 className="text-left mt-3 ">Welcome To <span style={{color:"#f9bc98" ,fontWeight:"bolder"}}><i>Libary Management</i></span></h1>
+        </div>
+        
+        </div>
+        </div>
+    </>
+  )
+}
+
+export default Home
